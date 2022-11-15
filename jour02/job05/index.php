20afcb8b6125fec624c5ba19dac50d0e02094567
jean-derieppe <jean.derieppe@laplateforme.io>
2022-11-15 18:41:22 +0100

    maj

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -1,17 +1,16 @@
-<?php 
-for ($n = 1; $n <= 100; $n++){
-    if ($n % 3 == 0 && $n % 5 == 0){
-        echo "Fizzbuzz" . "<br>" ;
-    }
-    elseif ($n % 3 == 0){
-        echo "Fizz" . "<br>" ;
-    }
-    elseif ($n % 5 == 0){
-        echo "Buzz" . "<br>" ;
-    }
-    else{
-        echo $n . "<br>" ;
-    }
+<?php
 
-}
+for ($n = 2; $n <= 1000; $n++){
+    $c=0;
+    for( $i = 2 ; $i <= $n/2 ; $i++) {
+        if ($n % $i == 0){
+            $c++;
+            break;
+        }
+    }
+    if($c == 0 && $n !=1){
+        echo $n . "<br>";
+    }
+  }
+ 
 ?>
