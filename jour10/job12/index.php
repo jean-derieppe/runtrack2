fda6ff6bf609ed2497d044a1c86408e72cb1e5b4
jean-derieppe <jean.derieppe@laplateforme.io>
2022-11-26 11:08:55 +0100

    job08 à job13 fini

---
+++ jour10/job12/index.php
@@ -0,0 +1,61 @@
+<?php
+//définir variable de connection a ma base de donnée SQL ( jour09) .
+$mysqli = new mysqli('localhost','root','','jour09');
+
+// requête Pour connaître le nombre de lignes totales dans une table .
+$request = $mysqli -> query ("select prenom, nom, naissance from etudiants where naissance between '1998-01-01' and '2018-12-31'");
+
+// affichage du résultat de la requête .
+//$resultat_all = $request -> fetch_all();
+//var_dump($resultat_all);
+
+//while (($resultat_array = $request -> fetch_array()) != null){
+//     var_dump($resultat_array);
+//}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        table,
+        th,
+        td {
+            padding: 10px;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+    <title>job01</title>
+
+</head>
+<body>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Prénom</th>
+                <th>Nom</th>
+                <th>Naissance</th>
+                
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while (($resultat = $request -> fetch_array()) != null){
+                echo "<tr>";
+                echo "<td>".$resultat['prenom']."</td>";
+                echo "<td>".$resultat['nom']."</td>";
+                echo "<td>".$resultat['naissance']."</td>";
+                echo "</tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+    
+</body>
+</html>
