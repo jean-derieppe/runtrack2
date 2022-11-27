4d465f6e0c297aeefc55f2ab6119ee67da2aa517
jean-derieppe <jean.derieppe@laplateforme.io>
2022-11-27 12:38:25 +0100

    job01 job02 du jour02 a jour

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,5 +1,5 @@
 <?php
-// déclaration d'une variable ($n) et de sa valeur cff; pour $n inférieur ou égal à 1337; et incrémenter 1 à $n pour chaque tour . //
+// déclaration d'une variable ($n) et de sa valeur (0); pour $n inférieur ou égal à 1337; incrémenter $n de 1 pour chaque tour jusqu'a 1337. //
 
 for ($n = 0; $n <= 1337; $n++){
     // si $n vaut 42, imprimer 42 en gras et souligné grace aux balises "<b><u>$n</u></br>" . //
--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -1,7 +1,7 @@
 <?php
 for ($n = 0; $n <= 1337; $n++ ){
     if ($n == 26 or $n == 37 or $n == 88 or $n == 1111) {
-    // Si $n vaut 26 ou 37 ou 88 ou 1111, alors ne rien imprimer ( "" ) et aller à la ligne avec "</br>" . //
+    // Si $n vaut 26 ou 37 ou 88 ou 1111, alors ne rien imprimer ( "" ) et aller à la ligne avec "</br>" ( grâce à la concaténation '.' ) . //
         echo "" . "</br>" ;
     } 
     else {
